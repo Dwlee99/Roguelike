6c76902acf86c4bf56a96d9e5130fda09eaf0868
Jack Farley <jlf329@cornell.edu>
2020-04-25 20:52:30 -0400

    Random board generation.

--- State.ml
+++ State.ml
@@ -1,4 +1,5 @@
 open Action
+open Random
 
 type tile = 
   | Player
@@ -54,12 +55,75 @@ let add_outer_walls board =
   done;
   board
 
+(** [empty_board width height] is an empty board with dimensions 
+    [width] and [height]. *)
+let empty_board width height =
+  Array.make_matrix width height Empty
+
+(** [randomize_tiles board] returns a board with the same with and height as 
+    [board] but with each tile having an equal chance of being [Empty] or 
+    [Wall].
+
+    NOTE: This function was inspired by the roguelike board generating algorithm
+    described on a blog post in Trystan's Blog 
+    (http://trystans.blogspot.com/2011/08/roguelike-tutorial-03-scrolling-through.html). *)
+let randomize_tiles board = 
+  Random.self_init ();
+  let width = Array.length board in
+  let height = Array.length board.(0) in
+  for x = 0 to width - 1 do
+    for y = 0 to height - 1 do
+      board.(x).(y) <- if Random.bool () = true then Empty else Wall;
+    done;
+  done;
+  board
+
+(** [smooth board] is the board [board] after undergoing one iteration of a 
+    wall-smoothing algorithm. 
+
+    NOTE: This function was inspired by the roguelike board generating algorithm
+    described on a blog post in Trystan's Blog 
+    (http://trystans.blogspot.com/2011/08/roguelike-tutorial-03-scrolling-through.html). *)
+let smooth board = 
+  let width = Array.length board in
+  let height = Array.length board.(0) in
+  let new_board = empty_board width height in
+  for x = 0 to width - 1 do
+    for y = 0 to height - 1 do
+      let empties = ref 0 in
+      let walls = ref 0 in
+      for ox = -1 to 1 do
+        for oy = -1 to 1 do
+          if x + ox < 0 || x + ox >= width || y + oy < 0 
+             || y + oy >= height
+          then ()
+          else if (board.(x + ox).(y + oy) = Empty)
+          then empties := 1 + !empties
+          else walls := 1 + !empties
+        done
+      done;
+      new_board.(x).(y) <- if empties >= walls then Empty else Wall;
+    done
+  done;
+  new_board
+
+
+(** [gen_board width height] is a randomly generated board with dimensions 
+    [width] and [height]. *)
+let gen_board width height =
+  let empty_board = empty_board width height in
+  let random_board = ref (randomize_tiles empty_board) in
+  for i = 0 to 7 do
+    random_board := smooth !random_board; 
+  done;
+  let board_with_walls = add_outer_walls !random_board in
+  board_with_walls
+
 
 let init_game width height =
   (** Make the game state random. Put walls in. Maybe randomize player spawn.  
   *)
-  let board = Array.make_matrix width height Empty in
-  let board = add_outer_walls board in
+  let board = gen_board width height in
   board.(width / 2).(height / 2) <- Player; 
   {
     board = board;
