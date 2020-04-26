1c5d7de87d8f42b94f27d0e6781cdfd7fc961e56
Derek Lee <dwl72@cornell.edu>
2020-04-25 21:14:58 -0400

    Created the break command

--- State.ml
+++ State.ml
@@ -23,7 +23,10 @@ type t = {
 
 (** Sets the tile at [(x, y)] to [tile]. *)
 let set_tile t (x, y) tile =
-  t.board.(x).(y) <- tile
+  let width = Array.length t.board in
+  let height = Array.length t.board.(0) in 
+  if x >= 0 && x < width && y >= 0 && y < height 
+  then t.board.(x).(y) <- tile
 
 (** Gets the contents of the tile at [(x, y)].
     If [(x, y)] is out of bounds, the result is [Wall]. *)
@@ -39,16 +42,41 @@ let move_player t (x, y) =
   set_tile t t.player.position Empty; set_tile t (x, y) Player;
   t.player.position <- (x, y)
 
+let up_one (x, y) = (x, y + 1)
+
+let down_one (x, y) = (x, y - 1)
+
+let right_one (x, y) = (x + 1, y)
+
+let left_one (x, y) = (x - 1, y)
+
+
 let update t action =
   (* Attack if enemy present. *)
-  let new_pos = match action with
-    | Up -> ((fun (x, y) -> (x, y + 1)) t.player.position)
-    | Down -> ((fun (x, y) -> (x, y - 1)) t.player.position)
-    | Left -> ((fun (x, y) -> (x - 1, y )) t.player.position)
-    | Right -> ((fun (x, y) -> (x + 1, y)) t.player.position)
-    | Rest -> t.player.position
-  in if get_tile t new_pos = Empty then (move_player t new_pos; t) else
+  match action with
+  | Move direction -> 
+    let new_pos = match direction with
+      | Up -> up_one t.player.position
+      | Down -> down_one t.player.position
+      | Left -> left_one t.player.position
+      | Right -> right_one t.player.position
+    in if get_tile t new_pos = Empty then (move_player t new_pos; t) else
+      t
+  | Break ->
+    for col = -1 to 1 do
+      for row = -1 to 1 do
+        let (x, y) = t.player.position in
+        let changed_pos = (x + col, y + row) in
+        if get_tile t changed_pos = Wall then set_tile t changed_pos Empty
+      done
+    done;
     t
+  | Rest -> t
+
+
+
+
+
 
 (** [add_outer_walls board] is a board identical to [board] except with
     each of the tiles that form the outer loop of the board being a wall. *)
--- action.ml
+++ action.ml
@@ -1,13 +1,19 @@
-type t = 
-  | Left
-  | Right
+type direction = 
   | Up
   | Down
+  | Left
+  | Right
+
+type t = 
+  | Move of direction
+  | Break
   | Rest
 
+
 let parse = function
-  | 'i' -> Up
-  | 'j' -> Left
-  | 'k' -> Down
-  | 'l' -> Right
+  | 'i' -> Move Up
+  | 'j' -> Move Left
+  | 'k' -> Move Down
+  | 'l' -> Move Right
+  | 'b' -> Break
   | _ -> Rest
