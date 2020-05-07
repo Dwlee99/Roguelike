2f1dfc6f9a49c5307478c8d06fcfc1fd29fbe1a8
Alexander Wang <aw576@cornell.edu>
2020-05-06 18:53:50 -0700

    made inventory ml and mli

---
+++ inventory.mli
@@ -0,0 +1,5 @@
+(** This module contains all the functions that manage the inventory of the
+    player. This includes their inventory, their equipped weapon(s) and any 
+    armor they are wearing.*)
+
+type t
