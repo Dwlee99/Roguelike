2f95adfc52e2186319ab587886da08f4aa959960
Alexander Wang <aw576@cornell.edu>
2020-05-06 21:11:50 -0700

    added inventory and armor systems

--- inventory.ml
+++ inventory.ml
@@ -12,3 +12,43 @@ type t = {
   max_items : int;
 }
 
+let init_inv = {
+  melee = None;
+  ranged = None;
+  head_prot = None;
+  torso_prot = None;
+  leg_prot = None;
+  feet_prot = None;
+  items = [];
+  max_items = 10;
+}
+
+let equip_weapon t w = 
+  match w.atk_type with
+  | Melee -> { t with melee = Some w; }
+  | Ranged -> { t with ranged = Some w; }
+
+let equip_armor t a = match a.a_type with
+  | Head -> {t with head_prot = Some a}
+  | Torso -> {t with torso_prot = Some a}
+  | Legs -> {t with leg_prot = Some a}
+  | Feet -> {t with feet_prot = Some a}
+
+exception MaxItemsReached
+
+let add_item t i = 
+  if List.length t.items < t.max_items then
+    {t with items = i :: t.items}
+  else
+    raise MaxItemsReached
+
+
+let remove_item t i =
+  if List.mem i t.items then 
+    {
+      t with
+      items = List.fold_left 
+          (fun acc x -> if x = i then acc else x :: acc) 
+          [] t.items;
+    }
+  else t
--- inventory.mli
+++ inventory.mli
@@ -1,5 +1,37 @@
+open Weapon
+open Armor
+
 (** This module contains all the functions that manage the inventory of the
     player. This includes their inventory, their equipped weapon(s) and any 
     armor they are wearing.*)
 
+(** The type of the inventory. This is a record containing the fields that
+    represent the inventory as a whole*)
 type t
+
+exception MaxItemsReached
+
+(** [init_inv] is a completely empty inventory with a starting value for
+    max_items set to 10*)
+val init_inv : t
+
+(** [equip_weapon t w] equips the weapon [w] into the melee or ranged
+    slot of the inventory [t] depending on the attack type of [w] *)
+val equip_weapon : t -> weapon -> t
+
+(** [equip_armor t a] equips the armor piece [a] into the correct
+    slot. If*)
+val equip_armor : t -> armor -> t
+
+(** [add_item t i] adds an item into the item list in t if the total
+    number of items is less than 10. Otherwise, it raises a 
+    MaxItemsReached exception*)
+val add_item : t -> string -> t (* change string to item when type is created*)
+
+(** [remove_item t i] removes the item i from inventory t if it exists
+    in t. If i is not in inventory t, the original inventory is returned
+    with no change*)
+val remove_item : t -> string -> t (*change string to item when type is created*)
+
+
+
