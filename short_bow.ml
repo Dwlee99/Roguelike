282f9591dbdb850a2d198bc8c5c1d5b07023a875
Derek Lee <dwl72@cornell.edu>
2020-05-09 21:04:38 -0400

    bow shoots in semi-circle

--- short_bow.ml
+++ short_bow.ml
@@ -1,4 +1,5 @@
 open Weapon
+open Math
 
 module Short_Bow : Weapon_Type = struct
 
@@ -11,11 +12,56 @@ module Short_Bow : Weapon_Type = struct
     level = level;
   }
 
+  (** The radius with which the bow can shoot. *)
+  let bow_radius = 7
+
+  (** [make_rect_coords leftX bottomY w h] lists the coordinates
+      [(leftX, bottomY)] through [(leftX + w - 1, bottomY + h - 1)]. *)
+  let make_rect_coords leftX bottomY w h =
+    let lst = ref [] in
+    for x = leftX to leftX + w - 1 do
+      for y = bottomY to bottomY + h - 1 do
+        lst := (x, y) :: !lst
+      done
+    done;
+    !lst
+
+  (** [in_radius coord r] is true if the coordinate is within [r] units of the
+      origin. *)
+  let in_radius coord r =
+    distance_sq coord (0,0) < (square bow_radius)
+
+  (** The coordinates matching the top semi-circle of the attack range. *)
+  let top_semi_circle = 
+    let rect = make_rect_coords (-bow_radius - 1) 1 (2 * bow_radius) 
+        (bow_radius + 1) in 
+    List.filter (fun c -> in_radius c bow_radius) rect
+
+  (** The coordinates matching the left semi-circle of the attack range. *)
+  let left_semi_circle = 
+    let rect = make_rect_coords (-bow_radius - 1) (-bow_radius - 1) 
+        (bow_radius + 1) (2 *bow_radius) in 
+    List.filter (fun c -> in_radius c bow_radius) rect
+
+  (** The coordinates matching the right semi-circle of the attack range. *)
+  let right_semi_circle = 
+    let rect = make_rect_coords 1 (-bow_radius + 1) (bow_radius + 1) 
+        (2 * bow_radius + 2) in 
+    List.filter (fun c -> in_radius c bow_radius) rect
+
+  (** The coordinates matching the bottom semi-circle of the attack range. *)
+  let bottom_semi_circle = 
+    let rect = make_rect_coords (-bow_radius - 1) (-bow_radius - 1) 
+        (2 * bow_radius + 2) (bow_radius + 1) in 
+    List.filter (fun c -> in_radius c bow_radius) rect
+
   let attack w dir = 
     match dir with
-    | Action.Up ->    List.init 5 (fun x -> (0, x, w.damage))
-    | Action.Down ->  List.init 5 (fun x -> (0, -x, w.damage))
-    | Action.Left ->  List.init 5 (fun x -> (-x, 0, w.damage))
-    | Action.Right -> List.init 5 (fun x -> (x, 0, w.damage))
+    | Action.Up ->    List.map (fun (x, y) -> (x, y, w.damage)) top_semi_circle
+    | Action.Down ->  List.map (fun (x, y) -> (x, y, w.damage)) 
+                        right_semi_circle
+    | Action.Left ->  List.map (fun (x, y) -> (x, y, w.damage)) left_semi_circle
+    | Action.Right -> List.map (fun (x, y) -> (x, y, w.damage)) 
+                        bottom_semi_circle
 
 end
