e993e7f5030ddf26fd6313f5542dcb5a0ef5b771
Derek Lee <dwl72@cornell.edu>
2020-05-09 21:15:59 -0400

    There is a minimum bow radius

--- short_bow.ml
+++ short_bow.ml
@@ -12,8 +12,11 @@ module Short_Bow : Weapon_Type = struct
     level = level;
   }
 
-  (** The radius with which the bow can shoot. *)
-  let bow_radius = 7
+  (** The outer radius with which the bow can shoot. *)
+  let bow_or = 7
+
+  (** The inner radius with which the bow cannot shoot. *)
+  let bow_ir = 2
 
   (** [make_rect_coords leftX bottomY w h] lists the coordinates
       [(leftX, bottomY)] through [(leftX + w - 1, bottomY + h - 1)]. *)
@@ -26,34 +29,41 @@ module Short_Bow : Weapon_Type = struct
     done;
     !lst
 
-  (** [in_radius coord r] is true if the coordinate is within [r] units of the
-      origin. *)
-  let in_radius coord r =
-    distance_sq coord (0,0) < (square bow_radius)
+  (** [in_radius coord inr outr] is true if the coordinate is within [or] units of 
+      the origin and greater than [ir] units of the origin. *)
+  let in_radius coord inr outr =
+    let b =
+      distance_sq coord (0,0) < (square outr)
+      && distance_sq coord (0, 0) > (square inr)
+    in print_string (string_of_bool b); b
+  (** [filter_in_radius lst] is the list [lst] filtered to only include 
+      coordinates within the inner and outer bow radius'. *)
+  let filter_in_radius lst = 
+    List.filter (fun c -> in_radius c bow_ir bow_or) lst
 
   (** The coordinates matching the top semi-circle of the attack range. *)
   let top_semi_circle = 
-    let rect = make_rect_coords (-bow_radius - 1) 1 (2 * bow_radius) 
-        (bow_radius + 1) in 
-    List.filter (fun c -> in_radius c bow_radius) rect
+    let rect = make_rect_coords (-bow_or - 1) 1 (2 * bow_or) 
+        (bow_or + 1) in 
+    filter_in_radius rect
 
   (** The coordinates matching the left semi-circle of the attack range. *)
   let left_semi_circle = 
-    let rect = make_rect_coords (-bow_radius - 1) (-bow_radius - 1) 
-        (bow_radius + 1) (2 *bow_radius) in 
-    List.filter (fun c -> in_radius c bow_radius) rect
+    let rect = make_rect_coords (-bow_or - 1) (-bow_or - 1) 
+        (bow_or + 1) (2 * bow_or) in 
+    filter_in_radius rect
 
   (** The coordinates matching the right semi-circle of the attack range. *)
   let right_semi_circle = 
-    let rect = make_rect_coords 1 (-bow_radius + 1) (bow_radius + 1) 
-        (2 * bow_radius + 2) in 
-    List.filter (fun c -> in_radius c bow_radius) rect
+    let rect = make_rect_coords 1 (-bow_or + 1) (bow_or + 1) 
+        (2 * bow_or + 2) in 
+    filter_in_radius rect
 
   (** The coordinates matching the bottom semi-circle of the attack range. *)
   let bottom_semi_circle = 
-    let rect = make_rect_coords (-bow_radius - 1) (-bow_radius - 1) 
-        (2 * bow_radius + 2) (bow_radius + 1) in 
-    List.filter (fun c -> in_radius c bow_radius) rect
+    let rect = make_rect_coords (-bow_or - 1) (-bow_or - 1) 
+        (2 * bow_or + 2) (bow_or + 1) in 
+    filter_in_radius rect
 
   let attack w dir = 
     match dir with
